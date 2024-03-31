4ce88d45956edf1261d01e81ee4dd2094ba0869c
Krystyna Shunkina <christina.shunkina@gmail.com>
2024-03-31 23:31:03 +0300

    gh pages

--- src/components/Description/Description.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-function Description() {
-    return (
-          <h1>Це сайт для підготовки на співбесіду на позицію фронтенд</h1>
-        
-    );
-}
-export default Description;
--- src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-import './styles.css'
-import React from 'react'
-import ReactDOM from 'react-dom/client';
-import App from './App.js';
-const mainEl = document.querySelector('body')
-const root = ReactDOM.createRoot(mainEl)
-root.render(<App />)
